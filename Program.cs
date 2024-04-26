b68f838d829757ea49be47f10344e1009184c5b6
sepa0 <sepa0@ZYAWA>
2024-04-26 17:40:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Configuration;
+
+namespace IPLogChecker
+{
+
+	internal class Program
+	{
+		static void Main ( string [ ] args )
+		{
+
+		}
+	}
+}
